adb5147cb26c739f1c2ade7ec93ccae60090bfe2
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2016-04-09 16:53:47 +0200

    Task Validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it 'can be built' do
-    task = Fabricate(:task)
+    task = Fabricate.build(:task)
     expect(task).not_to be_nil
+  end
+
+  it 'must have name' do
+    task = Fabricate.build(:task, name: nil)
+    expect(task).not_to be_valid
+    expect(task.errors[:name].any?).to be_truthy
   end
 
 end
